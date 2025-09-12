3488ad38c5246f142512579f2545c2d5b8903129
jannikgh <jannik.gross-hardt@iu-study.org>
2025-09-12 22:30:40 +0200

    Add files via upload

---
+++ dataflow_pipeline_local.py
@@ -0,0 +1,45 @@
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions, GoogleCloudOptions, StandardOptions
+import csv
+import logging
+
+class ParseCSVLine(beam.DoFn):
+    def process(self, element):
+        try:
+            fields = list(csv.reader([element]))[0]
+            return [{
+                'date': fields[0],
+                'number_of_strikes': int(fields[1]),
+                'center_point_geom': fields[2]
+            }]
+        except Exception as e:
+            logging.error(f"Fehler beim Parsen: {e}")
+            return []
+
+def run():
+    options = PipelineOptions()
+    google_cloud_options = options.view_as(GoogleCloudOptions)
+    google_cloud_options.project = 'agile-bonbon-470410-j2'
+    google_cloud_options.region = 'europe-west3'
+    google_cloud_options.job_name = 'csv-to-bq-blitzdaten'
+    google_cloud_options.staging_location = 'gs://blitzdaten_us1/tmp/staging'
+    google_cloud_options.temp_location = 'gs://blitzdaten_us1/tmp'
+    options.view_as(StandardOptions).runner = 'DirectRunner'
+
+    with beam.Pipeline(options=options) as pipeline:
+        (
+            pipeline
+            | 'CSV lesen' >> beam.io.ReadFromText('/Users/jannikgross-hardt/Desktop/Batch-basierte-Auswertung-von-Blitzdaten/lightning_strikes_dataset.csv', skip_header_lines=1)
+            | 'Parsen' >> beam.ParDo(ParseCSVLine())
+            | 'Nach BigQuery schreiben' >> beam.io.WriteToBigQuery(
+                table='blitzdaten.lightning_strikes',
+                dataset='blitzdaten',
+                project='agile-bonbon-470410-j2',
+                schema='date:DATE,number_of_strikes:INTEGER,center_point_geom:STRING',
+                write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND,
+                create_disposition=beam.io.BigQueryDisposition.CREATE_NEVER
+            )
+        )
+
+if __name__ == '__main__':
+    run()
